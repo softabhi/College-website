9e52403293dfa538889a3bd64c46b712f51a2638
softabhi <kdhar499@gmail.com>
2022-12-06 18:24:36 +0530

    add new fl

--- addform/index.php
+++ addform/index.php
@@ -16,6 +16,7 @@
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css"
         rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    
     <link rel="stylesheet" href="cssfolder/indexphp.css">
 </head>
 
