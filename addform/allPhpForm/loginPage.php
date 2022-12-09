47f5a8d62119712e9ceb598f5f0c7f42f248c2dc
softabhi <99878096+softabhi@users.noreply.github.com>
2022-12-09 11:22:34 +0530

    new pic added

---
+++ addform/allPhpForm/loginPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+      <div class="container mt-5" >
+        <button type="button" class="btn btn-primary" data-bs-target="#myModal" data-bs-toggle="modal" >Submit</button>
+        <div class="modal" id="myModal" >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="model-title">Contact Us</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+</body>
+</html>
